7a1e228d22de52e68251fbc7fa3c1e61130ddc2f
samanthaayeung <yeunghiuching21@gmail.com>
2025-12-27 16:16:44 -0800

    back to top button

--- js/main.js
+++ js/main.js
@@ -102,3 +102,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+  const backToTopButton = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTopButton.classList.add("show");
+    } else {
+      backToTopButton.classList.remove("show");
+    }
+  });
+
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
